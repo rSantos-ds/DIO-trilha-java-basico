cb0811a041808abf3ff849a1b5ade60768f591b8
Rafael Santos <rafa.dss@outlook.com>
2025-02-06 15:58:55 -0300

    primeiro commit

---
+++ src/main/java/ContaBanco/ContaTerminal.java
@@ -0,0 +1,32 @@
+package ContaBanco;
+
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+
+        System.out.print("Informe o número da conta: ");
+        numero = entrada.nextInt();
+        entrada.nextLine();
+
+        System.out.print("Informe a agência (padrão: xxx-x): ");
+        agencia = entrada.nextLine();
+
+        System.out.print("Informe o nome do cliente: ");
+        nomeCliente = entrada.nextLine();
+
+        System.out.print("Informe o saldo da conta: ");
+        saldo = entrada.nextDouble();
+        entrada.nextLine();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ",conta " +
+                numero + " e seu saldo " + saldo + " já está disponivel para saque");
+    }
+}
